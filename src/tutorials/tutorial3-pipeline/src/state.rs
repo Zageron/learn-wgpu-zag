4f5b9c43eccf8cae5c39b92cf5a7f1321fcc57c5
Zageron <hello@adambryant.ca>
2020-11-23 01:33:52 -0800

    Add render pipeline, and .spv to ignore.

--- src/tutorials/tutorial3-pipeline/src/state.rs
+++ src/tutorials/tutorial3-pipeline/src/state.rs
@@ -11,6 +11,7 @@ mod state {
         swap_chain: wgpu::SwapChain,
         size: winit::dpi::PhysicalSize<u32>,
         clear_color: wgpu::Color,
+        render_pipeline: wgpu::RenderPipeline,
     }
 
     impl State {
@@ -53,7 +54,7 @@ mod state {
                 .await
                 .unwrap();
 
-            let swap_chain_descriptor = wgpu::SwapChainDescriptor {
+            let swap_chain_descriptor: wgpu::SwapChainDescriptor = wgpu::SwapChainDescriptor {
                 usage: wgpu::TextureUsage::OUTPUT_ATTACHMENT,
                 format: wgpu::TextureFormat::Bgra8UnormSrgb,
                 width: size.width,
@@ -61,14 +62,62 @@ mod state {
                 present_mode: wgpu::PresentMode::Fifo,
             };
 
-            let swap_chain = device.create_swap_chain(&surface, &swap_chain_descriptor);
+            let swap_chain: wgpu::SwapChain =
+                device.create_swap_chain(&surface, &swap_chain_descriptor);
 
-            let clear_color = wgpu::Color {
+            let clear_color: wgpu::Color = wgpu::Color {
                 r: 0.1,
                 g: 0.2,
                 b: 0.3,
                 a: 0.0,
             };
+
+            // Shader Pipeline
+            let vs_module = device.create_shader_module(wgpu::include_spirv!("shader.vert.spv"));
+            let fs_module = device.create_shader_module(wgpu::include_spirv!("shader.frag.spv"));
+
+            let render_pipeline_layout =
+                device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                    label: Some("Render Pipeline Layout"),
+                    bind_group_layouts: &[],
+                    push_constant_ranges: &[],
+                });
+
+            let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+                label: Some("Render Pipeline"),
+                layout: Some(&render_pipeline_layout),
+                vertex_stage: wgpu::ProgrammableStageDescriptor {
+                    module: &vs_module,
+                    entry_point: "main",
+                },
+                fragment_stage: Some(wgpu::ProgrammableStageDescriptor {
+                    module: &fs_module,
+                    entry_point: "main",
+                }),
+                rasterization_state: Some(wgpu::RasterizationStateDescriptor {
+                    front_face: wgpu::FrontFace::Ccw,
+                    cull_mode: wgpu::CullMode::Back,
+                    depth_bias: 0,
+                    depth_bias_slope_scale: 0.0,
+                    depth_bias_clamp: 0.0,
+                    clamp_depth: false,
+                }),
+                color_states: &[wgpu::ColorStateDescriptor {
+                    format: swap_chain_descriptor.format,
+                    color_blend: wgpu::BlendDescriptor::REPLACE,
+                    alpha_blend: wgpu::BlendDescriptor::REPLACE,
+                    write_mask: wgpu::ColorWrite::ALL,
+                }],
+                primitive_topology: wgpu::PrimitiveTopology::TriangleList,
+                depth_stencil_state: None,
+                vertex_state: wgpu::VertexStateDescriptor {
+                    index_format: wgpu::IndexFormat::Uint16,
+                    vertex_buffers: &[],
+                },
+                sample_count: 1,
+                sample_mask: !0,
+                alpha_to_coverage_enabled: false,
+            });
 
             return Self {
                 surface,
@@ -78,6 +127,7 @@ mod state {
                 swap_chain,
                 size,
                 clear_color,
+                render_pipeline,
             };
         }
 
@@ -94,7 +144,9 @@ mod state {
             return false;
         }
 
-        pub fn update(&mut self) {
+        pub fn update(&mut self) {}
+
+        pub fn render(&mut self) {
             let frame = self
                 .swap_chain
                 .get_current_frame()
@@ -108,7 +160,7 @@ mod state {
                 });
 
             {
-                let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
                     color_attachments: &[wgpu::RenderPassColorAttachmentDescriptor {
                         attachment: &frame.view,
                         resolve_target: None,
@@ -119,14 +171,13 @@ mod state {
                     }],
                     depth_stencil_attachment: None,
                 });
+
+                render_pass.set_pipeline(&self.render_pipeline);
+                render_pass.draw(0..3, 0..1);
             }
+            // {} drop(_render_pass);
 
             self.queue.submit(std::iter::once(encoder.finish()));
-        }
-        // {} drop(_render_pass);
-
-        pub fn render(&mut self) {
-            //todo!()
         }
     }
 }
